9e3a14267742087f55589138e87ae998c8fc002d
IDaniil228 <99666220+IDaniil228@users.noreply.github.com>
2025-02-05 00:28:52 +0300

    added models

--- articles/settings.py
+++ articles/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
+    'web'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "web.CustomUser"
--- web/models.py
+++ web/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    login = models.CharField(max_length= 256, unique = True)
+    name = models.CharField(max_length= 256, null=False)
+    surname = models.CharField(max_length= 256, null=False)
+    subscriptions = models.ManyToManyField(settings.AUTH_USER_MODEL);
+
+
+class Articles(models.Model):
+    title = models.CharField(max_length=256, null=False)
+    create_date = models.DateField(null=False)
+    content = models.TextField()
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE());
