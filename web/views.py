2bd8156205fb1fcbe8e8dfe161883ff7790248b5
IDaniil228 <99666220+IDaniil228@users.noreply.github.com>
2025-02-13 19:52:20 +0300

    Make first html page

--- web/views.py
+++ web/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def main_view(response):
+    integer = 10
+    return render(response, "web/main.html", {
+        "int" : integer
+    })
