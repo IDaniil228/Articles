9dfbeed4fcc43c2c3ef6d9d82f1e8a9a6c7ec785
IDaniil228 <99666220+IDaniil228@users.noreply.github.com>
2025-03-03 23:21:30 +0300

    Add user photo

--- web/urls.py
+++ web/urls.py
@@ -5,6 +5,7 @@ from web.views import *
 urlpatterns = [
     path('', main_view, name="main"),
     path('registration/', registration_view, name="registration"),
+    path('edit/', registration_view, name="edit_profile"),
     path('auth/', auth_view, name="auth"),
     path('logout/', logout_view, name="logout"),
     path('articles/create', articles_view, name="articles_create"),
--- web/views.py
+++ web/views.py
@@ -1,7 +1,8 @@
 from collections import defaultdict
+from gc import get_objects
 
 from django.contrib.auth import authenticate, login, logout
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 
 from web.forms import RegistrationForm, AuthorizationForm, ArticlesForm
@@ -21,16 +22,20 @@ def main_view(request):
 def registration_view(request):
     form = RegistrationForm()
     if request.method == "POST":
-        form = RegistrationForm(data=request.POST)
+        form = RegistrationForm(data=request.POST, files=request.FILES)
         if form.is_valid():
-            user = CustomUser(
-                login = form.cleaned_data['login'],
-                name = form.cleaned_data['name'],
-                surname = form.cleaned_data['surname']
-            )
-            user.set_password(form.cleaned_data['password'])
-            user.save()
-            return redirect("main")
+            if CustomUser.objects.filter(login=form.cleaned_data['login']):
+                form.add_error("login", "Такой логин уже существует")
+            else:
+                    user = CustomUser(
+                        login=form.cleaned_data['login'],
+                        name=form.cleaned_data['name'],
+                        surname=form.cleaned_data['surname'],
+                        photo=form.cleaned_data['photo']
+                    )
+                    user.set_password(form.cleaned_data['password'])
+                    user.save()
+                    return redirect("main")
     return render(request, "web/registration.html", {
         "form" : form
     })
@@ -57,7 +62,7 @@ def logout_view(request):
 
 
 def articles_view(request, id = None):
-    articles = Articles.objects.get(id=id) if id is not None else None
+    articles = get_object_or_404(Articles, id=id) if id is not None else None
     form = ArticlesForm(instance=articles)
     if request.method == "POST":
         form = ArticlesForm(data=request.POST, files=request.FILES, instance=articles, initial={"user" : request.user})
@@ -69,7 +74,7 @@ def articles_view(request, id = None):
     })
 
 def viewing_articles_view(request, id):
-    article = Articles.objects.get(id=id)
+    article = get_object_or_404(Articles, id=id)
     return render(request, "web/viewing_articels.html", {
         "article" : article
     })
@@ -82,16 +87,18 @@ def other_authors_view(request):
     })
 
 def check_profile_view(request, id):
+    sub_user = request.user.subscriptions.filter(id=id)
+    already_sub = True if sub_user else False
     user = CustomUser.objects.get(id=id)
     articles = Articles.objects.filter(user=user)
-    print(articles)
     return render(request, "web/check_profile.html", {
         "user" : user,
-        "articles" : articles
+        "articles" : articles,
+        "already_sub" : already_sub
     })
 
 
 def subscribe_view(request, id):
-    user = CustomUser.objects.get(id=id)
+    user = get_object_or_404(CustomUser, id=id)
     request.user.subscriptions.add(user)
     return redirect("other_authors")
